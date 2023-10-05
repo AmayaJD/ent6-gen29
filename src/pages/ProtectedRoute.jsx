4056835916a04d3d2f97a4baad44fd095feb0853
Jose amaya <jose_1341986@hotmail.com>
2023-10-05 01:52:38 -0400

    hola

--- src/pages/ProtectedRoute.jsx
+++ src/pages/ProtectedRoute.jsx
@@ -1,16 +1,14 @@
-import { useSelector } from 'react-redux'
 import { Navigate, Outlet } from 'react-router-dom'
-import { credentialsSlice } from '../store/slices/credencials.slice'
 import HeaderMusic from '../components/shared/HeaderMusic'
 
 const ProtectedRoute = () => {
-  
-    const credential = useSelector(store => credentialsSlice)  
+   
 
-  if (credential !== null) {
-    return <> 
-      <HeaderMusic/> <Outlet/> 
-    </>
+  if (localStorage.getItem('token') !== null) {
+    return (<> 
+      <HeaderMusic/> 
+      <Outlet/> 
+    </>)
   } else { 
     return <Navigate to='/auth/login' />
   }
